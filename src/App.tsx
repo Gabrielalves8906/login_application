ce5aab13e41b255a687c0e4b9ed425b3a5bb97d0
Gabriel Rodrigues Alves <gra102667@gmail.com>
2024-04-16 17:41:26 -0300

    restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-
+import { Light } from "@mui/icons-material";
+import { Button, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { LightTheme } from "./shared/themes";
 
 export const App =()=> {
   return (
-    <div className="App">
-      Teste
-</div>
+    <ThemeProvider theme={LightTheme} >
+    <BrowserRouter>
+      <div className="App">
+        <Button variant="contained" color='primary' >Teste</Button>
+      </div>
+    </BrowserRouter>
+    </ThemeProvider>
   );
 } 
